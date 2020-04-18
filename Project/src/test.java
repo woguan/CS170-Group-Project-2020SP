21ece088993c0d074505d6253934044f3905d849
giani <giani@LAPTOP-PRQ8297G>
2020-04-18 16:51:03 -0700

    help

--- Project/src/test.java
+++ Project/src/test.java
@@ -3,6 +3,7 @@ import javax.swing.JOptionPane;
 public class test {
 	public static void main(String[] args) {
 		JOptionPane.showMessageDialog(null, "Sup", null, 0);
+		//help icant do java
 
 	}
 }
