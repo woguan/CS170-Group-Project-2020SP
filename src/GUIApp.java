3decae82a6801abdf50a3ee66ee312601d6123e0
Joshua <joshua.duan47@gmail.com>
2020-04-19 12:22:00 -0700

    Main method

---
+++ src/GUIApp.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class GUIApp {
+
+	public static void main(String[] args) {
+		
+		String number; 
+		number = JOptionPane.showInputDialog("How many numbers do you want?: ", null);
+
+	}
+
+}
